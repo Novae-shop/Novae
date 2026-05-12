9d7b51778af079465bc28503de80b411b3c08fcc
bricemedannou55 <bricemedannou55@gmail.com>
2026-05-12 02:25:22 +0000

    Modification du contenu

--- app.js
+++ app.js
@@ -141,4 +141,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   updateCartCount();
 
+  const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
 });
--- boutique.js
+++ boutique.js
@@ -116,489 +116,484 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       id: 1,
       name: "T-shirt Adidas",
-      price: 10000,
+      price: 4500,
       category: "clothes",
       image:"images/Vêtement/habit1.jpeg"
     },
     {
       id: 2,
       name: "T-shirt Casablanca",
-      price: 10000,
+      price: 4500,
       category: "clothes",
       image:"images/Vêtement/habit2.jpeg"
     },
         {
       id: 3,
       name: "T-shirt Armani",
-      price: 10000,
+      price: 4500,
       category: "clothes",
       image:"images/Vêtement/habit3.jpeg"
     },
         {
       id: 4,
       name: "T-shirt Brubrry",
-      price: 10000,
+      price: 4500,
       category: "clothes",
       image:"images/Vêtement/habit4.jpeg"
     },
         {
       id: 5,
       name: "T-shirt Jordan",
-      price: 10000,
+      price: 4500,
       category: "clothes",
       image:"images/Vêtement/habit5.jpeg"
     },
         {
       id: 6,
       name: "T-shirt  Balenciaga",
-      price: 10000,
+      price: 4500,
       category: "clothes",
       image:"images/Vêtement/habit6.jpeg"
     },
         {
       id: 7,
       name: "T-shirt Puma",
-      price: 10000,
+      price: 4500,
       category: "clothes",
       image:"images/Vêtement/habit7.jpeg"
     },
         {
       id: 8,
-      name: "   T-shirt B",
-      price: 10000,
+      name: "Balenciaga B Flames Oversized Reverse T-Shirt",
+      price: 4500,
       category: "clothes",
       image:"images/Vêtement/habit8.jpeg"
     },
         {
-      id: 3,
+      id: 9,
       name: "T-shirt Welldone",
-      price: 10000,
+      price: 4500,
       category: "clothes",
       image:"images/Vêtement/habit9.jpeg"
     },
         {
-      id: 3,
+      id: 10,
       name: "T-shirt Celine",
-      price: 10000,
+      price: 4500,
       category: "clothes",
       image:"images/Vêtement/habit10.jpeg"
     },
         {
-      id: 3,
+      id: 11,
       name: "T-shirt alo",
-      price: 10000,
+      price: 4500,
       category: "clothes",
       image:"images/Vêtement/habit11.jpeg"
     },
         {
-      id: 3,
-      name: "Habit complet disign Noir",
-      price: 10000,
+      id: 12,
+      name: "2 pièces chemise à manches courtes et pantalon pour hommes Noir",
+      price: 9000,
       category: "clothes",
       image:"images/Vêtement/habit12.jpeg"
     },
         {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 13,
+      name: "2 pièces chemise à manches courtes et pantalon pour hommes beige",
+      price: 9000,
       category: "clothes",
       image:"images/Vêtement/habit13.jpeg"
     },
         {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 14,
+      name: "Combinaison T-shirt et Short pour hommes Noir Motif grec",
+      price: 9000,
       category: "clothes",
       image:"images/Vêtement/habit14.jpeg"
     },
         {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 15,
+      name: "Combinaison T-shirt et Short pour hommes Blanc sur noir",
+      price: 9000,
       category: "clothes",
       image:"images/Vêtement/habit15.jpeg"
     },
         {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 16,
+      name: "Combinaison T-shirt et Short pour hommes Blanc sur bleu",
+      price: 9000,
       category: "clothes",
       image:"images/Vêtement/habit16.jpeg"
     },
         {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 17,
+      name: "Combinaison T-shirt et Short pour hommes Blanc sur vert",
+      price: 9000,
       category: "clothes",
       image:"images/Vêtement/habit17.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 18,
+      name: "Combinaison T-shirt et Short pour hommes",
+      price: 9000,
       category: "clothes",
       image:"images/Vêtement/habit18.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 19,
+      name: "Combinaison T-shirt manche longue et Short pour hommes",
+      price: 9000,
       category: "clothes",
       image:"images/Vêtement/habit19.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 20,
+      name: "T-shirt Chrome hearts sans manche pour hommes",
+      price: 4500,
       category: "clothes",
       image:"images/Vêtement/habit20.jpeg"
     },
         {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 21,
+      name: "T-shirt Louis vuitton sans manche pour hommes",
+      price: 4500,
       category: "clothes",
       image:"images/Vêtement/habit21.jpeg"
     },
+
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
-      category: "clothes",
-      image:"images/Vêtement/habit1.jpeg"
-    },
-    {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 23,
+      name: "Chemise Fleurie pour hommes Bleu",
+      price: 5000,
       category: "clothes",
       image:"images/Vêtement/habit22.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 24,
+      name: "Chemise de Bowling Tourbillon de Peinture – Lazy Oaf",
+      price: 5000,
       category: "clothes",
       image:"images/Vêtement/habit23.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 25,
+      name: "Chemise Fleurie pour hommes Rouge bordeaux",
+      price: 5000,
       category: "clothes",
       image:"images/Vêtement/habit24.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 26,
+      name: "Chemise en coton Bleue Hawaïenne Fleurie pour hommes",
+      price: 5000,
       category: "clothes",
       image:"images/Vêtement/habit25.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 27,
+      name: "une chemise à manches courtes avec un design color-block pour hommes",
+      price: 5000,
       category: "clothes",
       image:"images/Vêtement/habit26.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 28,
+      name: "sweat-shirts à manches longues avec un logo graphique stylisé sur le devant",
+      price: 7000,
       category: "clothes",
       image:"images/Vêtement/habit27.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 29,
+      name: "sweat-shirts à manches longues Supreme",
+      price: 7000,
       category: "clothes",
       image:"images/Vêtement/habit28.jpeg"
     },
    {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 30,
+      name: "sweat-shirts à manches longues Casablanca",
+      price: 7000,
       category: "clothes",
       image:"images/Vêtement/habit29.jpeg"
     },
    {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 31,
+      name: "Sweat-shirts à manches longues Chrome hearts",
+      price: 7000,
       category: "clothes",
       image:"images/Vêtement/habit30.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 32,
+      name: "Sweat-shirts à manches longues Chrome hearts",
+      price: 7000,
       category: "clothes",
       image:"images/Vêtement/habit31.jpeg"
     },
     
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 33,
+      name: "Alviero Martini - Ensemble de shorts en coton beige et blanc pour hommes",
+      price: 8000,
       category: "clothes",
       image:"images/Vêtement/habit32.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
-      category: "clothes",
-      image:"images/Vêtement/habit34.jpeg"
-    },
-    {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 34,
+      name: "Alviero Martini - Ensemble de shorts en coton vert et blanc pour hommes",
+      price: 8000,
       category: "clothes",
       image:"images/Vêtement/habit33.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 35,
+      name: "chemise à manches courtes avec col cubain et motif texturé couleur jaune moutarde",
+      price: 8000,
+      category: "clothes",
+      image:"images/Vêtement/habit34.jpeg"
+    },
+    {
+      id: 36,
+      name: "chemise à manches courtes avec col cubain et motif texturé couleur bleu",
+      price: 8000,
       category: "clothes",
       image:"images/Vêtement/habit35.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 37,
+      name: "chemise à manches courtes avec col cubain et motif texturé couleur noir",
+      price: 8000,
       category: "clothes",
       image:"images/Vêtement/habit36.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 38,
+      name: "chemise à manches courtes avec col cubain et motif texturé couleur rouge bordeaux",
+      price: 8000,
       category: "clothes",
       image:"images/Vêtement/habit37.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 39,
+      name: "chemise à manches courtes avec col cubain et motif texturé couleur beige",
+      price: 8000,
       category: "clothes",
       image:"images/Vêtement/habit38.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 40,
+      name: "maillots Corteiz Allstarz",
+      price: 8000,
       category: "clothes",
       image:"images/Vêtement/habit39.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 41,
+      name: "T-shirt oversize à manches courtes",
+      price: 8000,
       category: "clothes",
       image:"images/Vêtement/habit40.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 42,
+      name: "T-shirt oversize à manches courtes Top self",
+      price: 8000,
       category: "clothes",
       image:"images/Vêtement/habit41.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 43,
+      name: "2 pièces chemise à sans manches et pantalon pour hommes beige",
+      price: 12000,
       category: "clothes",
       image:"images/Vêtement/habit42.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 44,
+      name: " Jogging Design Rayures courbées sur les côtés",
+      price: 7000,
       category: "clothes",
       image:"images/Vêtement/habit43.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 45,
+      name: "Jogging Disign Chrome hearts",
+      price: 7000,
       category: "clothes",
       image:"images/Vêtement/habit44.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 46,
+      name: "Jogging Disign RickFaker",
+      price: 7000,
       category: "clothes",
       image:"images/Vêtement/habit45.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 47,
+      name: "Pantalons Needles Noir",
+      price: 7000,
       category: "clothes",
       image:"images/Vêtement/pat1.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 48,
+      name: "Pantalons Needles Gris",
+      price: 7000,
       category: "clothes",
       image:"images/Vêtement/pat2.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 49,
+      name: "Short Jean pour hommes", 
+      price: 7000,
       category: "clothes",
       image:"images/Vêtement/habit46.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 50,
+      name: "Maillot NFL Nike Rouge",
+      price: 6000,
       category: "clothes",
       image:"images/Vêtement/habit47.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 51,
+      name: " Maillot NFL Nike Jaune",
+      price: 6000,
       category: "clothes",
       image:"images/Vêtement/habit48.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 52,
+      name: "Ensemble survêtement (hoodie + pantalon) Broken Planet bleu clair",
+      price: 13000,
       category: "clothes",
       image:"images/Vêtement/habit49.jpeg"
     },
     {
-      id: 3,
-      name: "T-shirt Noir",
-      price: 10000,
+      id: 53,
+      name: "Ensemble survêtement (hoodie + pantalon) Broken Planet Noir",
+      price: 13000,
       category: "clothes",
       image:"images/Vêtement/habit50.jpeg"
     },
+
 {
-  id: 4,
+  id: 54,
   name: "IPhone 12ProMax 256Gb",
   price: 200000,
   category: "phones",
   image: "images/phones/iphone1.webp"
 },
 {
-  id: 5,
+  id: 55,
   name: "IPhone XR 64Gb",
   price: 100000,
   category: "phones",
   image: "images/phones/iphone12.jpg"
 },
 {
-  id: 6,
+  id: 56,
   name: "IPhone XR 256Gb",
   price: 120000,
   category: "phones",
   image: "images/phones/iphone14.jpg"
 },
 {
-  id: 7,
+  id: 57,
   name: "IPhone 11 64Gb",
   price: 130000,
   category: "phones",
-  image: "images/phones/iphone11.png"
+  image: "images/phones/iphone11.jpg"
 },
 {
-  id: 13,
+  id: 58,
   name: "IPhone 11 128Gb ",
   price: 140000,
   category: "phones",
   image: "images/phones/iphone10.png"
 },
 {
-  id: 14,
+  id: 59,
   name: "IPhone 12 64Gb",
   price: 130000,
   category: "phones",
   image: "images/phones/iphone9.jpg"
 },
 {
-  id: 15,
+  id: 60,
   name: "IPhone 13 256Gb",
   price: 190000,
   category: "phones",
   image: "images/phones/iphone8.jpg"
 },
 {
-  id: 16,
+  id: 61,
   name: "IPhone 13 128Gb",
   price: 200000,
   category: "phones",
   image: "images/phones/iphone7.jpg"
 },
 {
-  id: 17,
+  id: 62,
   name: "IPhone 13Pro 128Gb",
   price: 240000,
   category: "phones",
   image: "images/phones/iphone6.jpg"
 },
 {
-  id: 17,
+  id: 63,
   name: "IPhone 13Pro 256Gb",
   price: 250000,
   category: "phones",
   image: "images/phones/iphone15.jpg"
 },
 {
-  id: 18,
+  id: 64,
   name: "IPhone 14 128Gb ",
   price: 220000,
   category: "phones",
   image: "images/phones/iphone5.jpg"
 },
 {
-  id: 18,
+  id: 65,
   name: "IPhone 14 128Gb ",
   price: 220000,
   category: "phones",
   image: "images/phones/iphones5.jpg"
 },
 {
-  id: 19,
+  id: 66,
   name: "IPhone 14Pro 128Gb",
   price: 270000,
   category: "phones",
   image: "images/phones/iphone4.jpg"
 },
 {
-  id: 20,
+  id: 67,
   name: "IPhone 14Pro 256Gb",
   price: 290000,
   category: "phones",
   image: "images/phones/iphones4.jpg"
 },
 {
-  id: 21,
+  id: 68,
   name: "IPhone 14ProMax 128Gb",
   price: 300000,
   category: "phones",
   image: "images/phones/iphone3.jpg"
 },
 {
-  id: 22,
+  id: 69,
   name: "IPhone 15ProMax 128Gb",
   price: 450000,
   category: "phones",
   image: "images/phones/iphone13.jpg"
 },
 {
-  id: 23,
+  id: 70,
   name: "Iphone 16 128Gb",
   price: 390000,
   category: "phones",
@@ -606,35 +601,35 @@ document.addEventListener("DOMContentLoaded", () => {
 },
 
 {
-  id: 25,
+  id: 71,
   name: " Pixel 6 128Gb",
   price: 100000,
   category: "phones",
   image: "images/phones/pixel2.jpeg"
 },
 {
-  id: 26,
+  id: 72,
   name: "Pixel 7 128Gb",
   price: 120000,
   category: "phones",
   image: "images/phones/pixel1.jpg"
 },
 {
-  id: 27,
+  id: 73,
   name: " Motorola G 04 128Gb",
   price: 60000,
   category: "phones",
   image: "images/phones/moto1.jpg"
 },
 {
-  id: 28,
+  id: 74,
   name: "Motorola G-5G 2025 64Gb",
   price: 60000,
   category: "phones",
   image: "images/phones/moto2.jpg"
 },
 {
-  id: 29,
+  id: 75,
   name: "Motorola G-5G 2025 128Gb",
   price: 70000,
   category: "phones",
